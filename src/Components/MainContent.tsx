0532c3dcebdcb9af5c251e47bd32e8c3f463d2f7
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-12-10 20:36:35 +0500

    feat: added a new component as a BookCard fetching data from API

--- src/Components/MainContent.tsx
+++ src/Components/MainContent.tsx
@@ -1,15 +1,75 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useFilter } from './FilterContext';
 import { Tally3 } from 'lucide-react';
+import axios from 'axios';
+import BookCard from './BookCard';
 
 const MainContent = () => {
-  const { searchQuery, selectedQuery, minPrice, maxPrice, keyword } =
+  const { searchQuery, selectedCategory, minPrice, maxPrice, keyword } =
     useFilter();
   const [products, setProducts] = useState<any[]>([]);
   const [filter, setFilter] = useState('all');
   const [currentPage, setCurrentPage] = useState(1);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const itemsPerPage = 12;
+
+  useEffect(() => {
+    let url = `https://dummyjson.com/products?limit=${itemsPerPage}&skip=${
+      (currentPage - 1) * itemsPerPage
+    }`;
+    if (keyword) {
+      url = `https://dummyjson.com/products/search?q=${keyword}`;
+    }
+
+    axios
+      .get(url)
+      .then((response) => {
+        setProducts(response.data.products);
+      })
+      .catch((error) => {
+        console.error('Error fetching data', error);
+      });
+  }, [currentPage, keyword]);
+
+  const getFilteredProducts = () => {
+    let filteredProducts = products;
+
+    if (selectedCategory) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.category === selectedCategory
+      );
+    }
+
+    if (minPrice !== undefined) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.price >= minPrice
+      );
+    }
+    if (maxPrice !== undefined) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.price <= maxPrice
+      );
+    }
+
+    if (searchQuery) {
+      filteredProducts = filteredProducts.filter((product) =>
+        product.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    switch (filter) {
+      case 'expensive':
+        return filteredProducts.sort((a, b) => b.price - a.price);
+      case 'cheap':
+        return filteredProducts.sort((a, b) => a.price - b.price);
+      case 'popular':
+        return filteredProducts.sort((a, b) => b.rating - a.rating);
+      default:
+        return filteredProducts;
+    }
+  };
+
+  const filteredProducts = getFilteredProducts();
   return (
     <section className="xl:x-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5">
       <div className="mb-5">
@@ -45,7 +105,29 @@ const MainContent = () => {
             )}
           </div>
         </div>
-        
+        <div className="grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 gap-5">
+          {filteredProducts.map((product) => (
+            <BookCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              image={product.thumbnail}
+              price={product.price}
+            />
+          ))}
+        </div>
+
+        <div className="flex flex-col sm:flex-row justify-between items-center mt-5">
+          {/* Previous */}
+
+          <button className="border px-4 py-2 mx-2 rounded-full">
+            Previous
+          </button>
+          <button className="border px-4 py-2 mx-2 rounded-full">Next</button>
+        </div>
+
+        {/* 1,2,3,4,5 */}
+        {/* Next */}
       </div>
     </section>
   );
