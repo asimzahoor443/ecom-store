9d84295549bad466621d7d436b794df9bf8a3b76
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-12-24 12:29:25 +0500

    feat:updated TopSellers.tsx

--- src/Components/TopSellers.tsx
+++ src/Components/TopSellers.tsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+
+interface Author {
+  name: string;
+  isFollowing: boolean;
+  image: string;
+}
 
 const TopSellers = () => {
+  const [authors, setAuthors] = useState<Author[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://randomuser.me/api/?results=5');
+
+        const data = await response.json();
+
+        const authorsData: Author[] = data.results.map((user: any) => ({
+          name: `${user.name.first} ${user.name.last}`,
+          isFollowing: false,
+          image: user.picture.medium,
+        }));
+      } catch (error) {
+        console.error(`Error fetching authors: ${error}`);
+      }
+    };
+  });
   return <div>TopSellers</div>;
 };
 
