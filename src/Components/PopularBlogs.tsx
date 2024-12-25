32a51c6329ff7365b27f45c699c8dccd4083df1f
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-12-25 12:34:53 +0500

    docs: updated PopularBlogs

--- src/Components/PopularBlogs.tsx
+++ src/Components/PopularBlogs.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { MessageCircle, ThumbsUp } from 'lucide-react';
 
 const PopularBlogs = () => {
-  return (
-    <div>PopularBlogs</div>
-  )
-}
+  const blogs = [
+    {
+      title: 'My Amazing Blog Title 1',
+      author: 'Jordan',
+      likes: 142,
+      comments: 44,
+    },
+    {
+      title: 'My Amazing Blog Title 2',
+      author: 'John',
+      likes: 153,
+      comments: 25,
+    },
+    {
+      title: 'My Amazing Blog Title 3',
+      author: 'Nathen',
+      likes: 100,
+      comments: 20,
+    },
+    {
+      title: 'My Amazing Blog Title 4',
+      author: 'Huxn',
+      likes: 50,
+      comments: 14,
+    },
+  ];
 
-export default PopularBlogs
+  return (
+    <div className="bg-white p-5 w-[25rem] mt-4 border ml-5 rounded">
+      <h2 className="text-xl font-bold mb-5">Popular Blogs</h2>
+      <ul>
+        {blogs.map((blog, index) => (
+          <li key={index} className="mb-4">
+            <div className="flex justify-between items-center">
+              <span className="font-bold mb-2">{blog.title}</span>
+            </div>
+            <span className="text-gray-600">Publish by {blog.author}</span>
+            <div className="flex items-center mt-2">
+              <MessageCircle size={16} />
+              <span className="text-gray-500 mr-5 ml-1">{blog.likes}</span>
+
+              <ThumbsUp />
+              <span className="text-gray-500 mr-2 ml-2">{blog.comments}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default PopularBlogs;
